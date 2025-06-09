d1ad6a2e47afe9d6f8cb0a092a085ffb62079f87
Halytskyi5 <volodyaforschool@gmail.com>
2025-06-09 18:27:00 +0300

    add details-block

--- src/app/new-page/new-page.component.ts
+++ src/app/new-page/new-page.component.ts
@@ -1,42 +1,14 @@
 import {Component, OnInit} from '@angular/core';
 import {ObserveVisibilityDirective} from '../observe-visibility.directive';
-import {WaIntersectionObservee, WaIntersectionObserverDirective} from '@ng-web-apis/intersection-observer';
-
 @Component({
   selector: 'app-new-page',
   imports: [
-    ObserveVisibilityDirective,
-    WaIntersectionObserverDirective,
-    WaIntersectionObservee
+    ObserveVisibilityDirective
   ],
   templateUrl: './new-page.component.html',
   styleUrl: './new-page.component.scss',
   standalone: true
 })
-export class NewPageComponent implements OnInit{
-  ngOnInit() {/*
-  let targets = document.querySelector('.time-block');
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-      entry.target.classList.toggle("is-visible", entry.isIntersecting)
-    })
-  })
-  observer.observe(targets);
-*/
-  }
-  onIntersection(entries: IntersectionObserverEntry[]) {
-    entries.forEach(entry => {
-      entry.target.classList.toggle("is-visible", entry.isIntersecting)
-      console.error(entry)
-    })
-  }
-  intersectImage(entries : IntersectionObserverEntry[]) {
-    //entries.
-    /*entries.forEach(entry => {
-      entry.target.classList.add("image-animation")
-
-    });*/
-  }
-
+export class NewPageComponent {
 
 }
