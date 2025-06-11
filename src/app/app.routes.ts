9810a3e65a830f07914d89db0a021f4431d6a942
Halytskyi5 <volodyaforschool@gmail.com>
2025-06-11 20:45:51 +0300

    delete home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,14 @@
 import { Routes } from '@angular/router';
-import {HomePageComponent} from './home-page/home-page.component';
 import {NewPageComponent} from './new-page/new-page.component';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'invite-you',
     pathMatch: 'full'
   },
   {
-    path: 'home',
-    component: HomePageComponent
-  },
-  {
-    path: 'new',
+    path: 'invite-you',
     component: NewPageComponent
   }
 ];
