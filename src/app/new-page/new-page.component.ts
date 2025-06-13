4c2272d3169bcc4bf23922cdf455e10be734169d
Halytskyi5 <volodyaforschool@gmail.com>
2025-06-13 22:36:08 +0300

    add block to confirm presence

--- src/app/new-page/new-page.component.ts
+++ src/app/new-page/new-page.component.ts
@@ -1,17 +1,34 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
 import {ObserveVisibilityDirective} from '../observe-visibility.directive';
+import {FormsModule} from '@angular/forms';
+import {NgClass, NgIf} from '@angular/common';
 @Component({
   selector: 'app-new-page',
   imports: [
-    ObserveVisibilityDirective
+    ObserveVisibilityDirective,
+    FormsModule,
+    NgIf,
+    NgClass
   ],
   templateUrl: './new-page.component.html',
   styleUrl: './new-page.component.scss',
   standalone: true
 })
 export class NewPageComponent {
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
+  name = '';
+  family = '';
+  side = '';
+  showConfirm = false;
   acceptPresence() {
-    console.log('worked')
+    this.showConfirm = true;
+    //this.renderer.addClass(this.el.nativeElement, "visible")
+    console.log('added')
+  }
+
+  submitForm() {
+    console.log(this.name + " " + this.family + " " + this.side)
+    this.showConfirm = false;
   }
 
 }
