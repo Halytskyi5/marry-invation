dd14d06ed67682f61462b452604c636768e6a6bf
Halytskyi5 <volodyaforschool@gmail.com>
2025-06-25 13:15:43 +0300

    added unsubscibe methods

--- src/app/new-page/new-page.component.ts
+++ src/app/new-page/new-page.component.ts
@@ -1,10 +1,11 @@
-import {Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
+import {Component, ElementRef, OnDestroy, OnInit, Renderer2, ViewChild} from '@angular/core';
 import {ObserveVisibilityDirective} from '../observe-visibility.directive';
 import {FormsModule} from '@angular/forms';
 import {NgClass, NgIf} from '@angular/common';
 import {AnswersService} from '../answers.service';
 import {Answer} from '../model/answer';
 import {Subscription} from 'rxjs';
+import {response} from 'express';
 @Component({
   selector: 'app-new-page',
   imports: [
@@ -17,12 +18,22 @@ import {Subscription} from 'rxjs';
   styleUrl: './new-page.component.scss',
   standalone: true
 })
-export class NewPageComponent {
+export class NewPageComponent implements OnInit, OnDestroy{
   constructor(private renderer: Renderer2, private el: ElementRef, private answerService : AnswersService) {}
   name = '';
   family = '';
   side = '';
   showConfirm = false;
+  addAnswerSubscription : Subscription;
+  wakeUpServer : Subscription;
+
+  ngOnInit(): void {
+    this.wakeUpServer = this.answerService.getAnswers().subscribe({
+      next: value => value,
+      error: err => err
+    });
+  }
+
   acceptPresence() {
     this.showConfirm = true;
   }
@@ -33,14 +44,12 @@ export class NewPageComponent {
       family : this.family,
       fromGroom: this.side === 'groom'
     }
-    this.answerService.addAnswer(answer).subscribe({
+    this.addAnswerSubscription = this.answerService.addAnswer(answer).subscribe({
       next: response => {
-        console.log(response);
         this.closeWindow()
       },
       error: err => {
         console.error(err);
-      }, complete () {
       }
     });
   }
@@ -48,5 +57,11 @@ export class NewPageComponent {
   closeWindow() {
     this.showConfirm = false;
   }
+
+  ngOnDestroy(): void {
+    this.wakeUpServer.unsubscribe();
+    this.addAnswerSubscription.unsubscribe();
+  }
+
 
 }
